9e011d6f9eba4a6b00580b8fb21825739759a924
Matt <mbrowne108@gmail.com>
2021-07-20 16:01:51 -0400

    commit tree c6cdc3ba3ff9cd9e02cb2e4d5df8de869beedf5b

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+let scuberHq = 42;
+let travelBlocks;
+
+function distanceFromHqInBlocks(pickupStreet) {
+    if (scuberHq > pickupStreet) {
+        let blocks = scuberHq - pickupStreet;
+        return blocks;
+    } else {
+        let blocks = pickupStreet - scuberHq;
+        return blocks;
+    }
+}
+
+function distanceFromHqInFeet(pickupStreet) {
+    return distanceFromHqInBlocks(pickupStreet) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start > end) {
+        travelBlocks = start - end
+        return travelBlocks * 264
+    } else {
+        travelBlocks = end - start
+        return travelBlocks * 264
+    }
+}
+
+
+function calculatesFarePrice(start, end) {
+    distanceTravelledInFeet(start, end);
+    if (travelBlocks * 264 <= 400) {
+        let fare = 0;
+        return fare;
+    }  else if (travelBlocks * 264 > 400 && travelBlocks * 264 <= 2000) {
+        let chargedFeet = travelBlocks * 264 - 400;
+        let fare = chargedFeet * 0.02;
+        if (chargedFeet > 0) {
+            return fare;
+        }  else {
+            return -fare;
+        }
+    }  else if (travelBlocks * 264 > 2000 && travelBlocks * 264 <= 2500) {
+        let fare = 25;
+        return fare;
+    }  else {
+        return 'cannot travel that far';
+    }
+}
